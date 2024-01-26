43a53e7faee485a07d63209877951fe7263c4bb2
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-01-26 17:32:08 +0530

    New Commit

--- Works_On_APIs/app.js
+++ Works_On_APIs/app.js
@@ -1,33 +1,42 @@
+// --------------------->> Showing weather report on Every city which you Enter <<-------------------------
 
-const show = document.querySelector("#show");
-const fact = document.querySelector("#fact");
+                //   --->Weather Report <---
 
-show.addEventListener('click',()=>{
-    getFact();
-})
-const getFact = async ()=>{
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Kahalgaon&appid=82e0e6a5e2b7df2e56550b3c128289e6";
-    console.log("Button Clicked...");
+let show = document.querySelector("#show");
+let fact = document.querySelector("#fact");
 
+show.addEventListener("click", () => {
+  showFact();
+});
+
+const showFact = async () => {
+  try {
+    console.log("You clicked the button");
+    let place = document.querySelector("#place").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=82e0e6a5e2b7df2e56550b3c128289e6`;
     const myurl = await fetch(url);
+    if (!myurl.ok) {
+      throw Error(myurl.statusText);
+    }
     console.log(myurl);
+    const urlJson = await myurl.json();
+    console.log(urlJson);
+    let Myweather = urlJson.weather[0].main;
+    let MyweatherIcon = urlJson.weather[0].icon;
 
-    const jsonUrl = await myurl.json();
-    console.log(jsonUrl.main)
-    console.log(jsonUrl.main.temp_min);
+    const temp = (urlJson.main.feels_like - 273.15).toFixed(2);
 
-    fact.innerHTML = `<h1>Min Temp is: ${(jsonUrl.main.feels_like-273.15)}'C</h1>` 
-}
-
-
-
-
+    fact.innerHTML = `<h2> Temperature of ${place} is: ${temp}  🌡️degree Celcius</h2> 
+                        <h3>Weather is : ${Myweather} </h3> ${MyweatherIcon}`;
+  } catch (err) {
+    console.log("You have errror in your code");
+  }
+};
 
 
 // -------------------------->>> Showing New Motivational Quotes on every click on Button <<<--------------------------
 
 // const url = "https://type.fit/api/quotes";
-
 
 // const fact = document.querySelector("#fact");
 // const myblock = document.querySelector('#myblock');
@@ -50,4 +59,3 @@ const getFact = async ()=>{
 //         myblock.innerHTML = "Sorry.. 😲 No more quotes available.";
 //     }
 // }
-
