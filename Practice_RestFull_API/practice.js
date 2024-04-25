35332992be3bb2080330318d36559b79d0a1b840
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-04-25 13:51:43 +0530

    New Commit

--- Practice_RestFull_API/practice.js
+++ Practice_RestFull_API/practice.js
@@ -1,11 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var app = (0, express_1.default)();
-var server = require('./MOCK_DATA.json'); // Assuming you have defined types for your server data
-app.get('/', function (req, res) {
-    return res.json(server);
-});
-app.listen(3000, function () {
-    console.log("Server is running at port 3000");
-});
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello this is the Home Page Server</h1>")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running at port 3000...")
+})
