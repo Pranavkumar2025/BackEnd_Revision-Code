5e55c10ee140488750b62c4d8e419b1e0dea9a90
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-04-29 11:11:01 +0530

    New Commit

--- Middleware/app.js
+++ Middleware/app.js
@@ -18,7 +18,6 @@ const logRequest = (req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 };
-
 // Apply middleware globally
 app.use(logRequest);
 
