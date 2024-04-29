843094c8d1a8e17008ecf4c14237ca883c117d6a
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-04-29 16:19:33 +0530

    New Commit

--- MongooseJs/mongoosePractice/del.js
+++ MongooseJs/mongoosePractice/del.js
@@ -1,32 +1,38 @@
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
 
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/practice');
+mongoose.connect("mongodb://localhost:27017/PranavMongo").then(()=>{
+    console.log("Connection Succesful...");
+}).catch((err)=>{
+    console.log("You have Some problem in your code You have to find your Code...",err);
+
+})
+
+
+const mySchema = new mongoose.Schema({
+    Name : {
+        type : String,
+        required : true
+    },
+    Branch : String,
+    Number : Number
+
+})
+
+
+const pranvFile = new mongoose.model("FileMode",mySchema);
+
+const createNewFile  = async ()=>{
+
+    const mydoc1 = await new pranvFile( {
+        Name : "Pranav Kumar",
+        Branch : "AIML",
+        Number : 2323
+    })
+
+    const data = await mydoc1.save();
+
+    console.log(data)
 }
 
-const kittySchema = new mongoose.Schema({
-    name: String
-});
-kittySchema.methods.speak = function speak() {
-    const greeting = this.name
-        ? 'Meow name is ' + this.name
-        : 'I don\'t have a name';
-    console.log(greeting);
-};
-const Kitten = mongoose.model('Kitten', kittySchema);
-
-const silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
-
-const fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak(); // "Meow name is fluffy"
-
-fluffy.save();
-fluffy.speak()
-
-const kittens = Kitten.find();
-console.log(kittens);
-
-Kitten.find({ name: /^fluff/ });
+createNewFile()
