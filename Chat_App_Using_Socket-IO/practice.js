662426461ce5222d05d94f16f0f1ee45b9d35344
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-05-05 13:47:41 +0530

    New Commit

--- Chat_App_Using_Socket-IO/practice.js
+++ Chat_App_Using_Socket-IO/practice.js
@@ -1,3 +1,26 @@
-import express from "express";
+const express = require('express');
+const http = require('http');
+const { join } = require('node:path');
+const {Server} = require('socket.io');
 
-import {createServer} from 'createServer';
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  }); 
+
+
+
+server.listen(3000,()=>{
+    console.log("Your server is running http://localhost:3000");
+})
